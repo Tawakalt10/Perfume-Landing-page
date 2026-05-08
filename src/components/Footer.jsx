6f0147ee3e49d8e8c5afaa98e984a77e8c9dae0e
Tawakalt10 <raheemtawa@gmail.comm>
2026-05-08 10:53:09 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,53 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <div>
+
+    <div className='footer-section'>
+      <div className='footer-first-section'>
+    <div className='footer-brandname'>
+      <h4 className="footer-heading">
+      Aurelle
+     </h4>
+     <p className="footer-para">Grasse, France</p>
+    </div>
+
+   <div>
+   
+    <ul className='footer-list'>
+      <li className='footer-links heading'>Shop</li>
+      <li className='footer-links'>fragrances</li>
+      <li className='footer-links'>Discovery set</li>
+      <li className='footer-links'>Gifts</li>
+    </ul>
+   </div>
+   </div>
+
+<div className='footer-second-section'>
+   <div>
+    <ul className='footer-list'>
+       <li className='footer-links heading'>House</li>
+      <li className='footer-links'>Our story</li>
+      <li className='footer-links'>Atelier</li>
+      <li className='footer-links'>Sustainaibility</li>
+    </ul>
+   </div>
+
+   <div>
+    <ul className='footer-list'>
+       <li className='footer-links heading'>Care</li>
+      <li className='footer-links'>Contact</li>
+      <li className='footer-links'>Shipping</li>
+      <li className='footer-links'>Returns</li>
+    </ul>
+   </div>
+  </div>
+  </div>
+
+    <p className='copyright'>  &copy;  2026 Maison Aurelle.@TeeCodes. All right reserved </p>
+   
+    </div>
   )
 }
 
